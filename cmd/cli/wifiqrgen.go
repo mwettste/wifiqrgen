7ead96ff4b1ea43621bb235c361975ce81c0982c
Marco <marco.wettstein@gmail.com>
2022-04-23 21:17:38 +0200

    allow user to specify encryption

--- cmd/cli/wifiqrgen.go
+++ cmd/cli/wifiqrgen.go
@@ -11,9 +11,11 @@ func main() {
 	var ssid string
 	var password string
 	var isHidden bool
+	var encryption string
 
 	flag.StringVar(&ssid, "ssid", "", "Defines the SSID of the Wifi (required)")
 	flag.StringVar(&password, "pass", "", "Defines the password of the Wifi (required)")
+	flag.StringVar(&encryption, "encryption", "WPA", "Encryption type of the Wifi [WPA|WEP] (default: WPA)")
 	flag.BoolVar(&isHidden, "hidden", false, "Set this if the SSID is hidden")
 	flag.Parse()
 
@@ -23,11 +25,17 @@ func main() {
 		return
 	}
 
+	if encryption != "WEP" && encryption != "WPA" {
+		fmt.Println("Invalid encryption type")
+		flag.PrintDefaults()
+		return
+	}
+
 	hiddenIdentifier := ""
 	if isHidden {
 		hiddenIdentifier = ";H:true"
 	}
-	qrcodeText := fmt.Sprintf("WIFI:T:WPA;S:%s;P:%s%s", ssid, password, hiddenIdentifier)
+	qrcodeText := fmt.Sprintf("WIFI:T:%s;S:%s;P:%s%s", encryption, ssid, password, hiddenIdentifier)
 
 	fmt.Println("Generating code...")
 
