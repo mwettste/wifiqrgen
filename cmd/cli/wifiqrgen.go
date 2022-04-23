07aa94946a74a6978011f054d2413652ccaacb1d
Marco <marco.wettstein@gmail.com>
2022-04-23 20:34:37 +0200

    verify required parameters are passed inform user otherwise

--- cmd/cli/wifiqrgen.go
+++ cmd/cli/wifiqrgen.go
@@ -11,9 +11,15 @@ func main() {
 	var ssid string
 	var password string
 
-	flag.StringVar(&ssid, "ssid", "", "Defines the SSID of the Wifi.")
-	flag.StringVar(&password, "pass", "", "Defines the password of the Wifi.")
+	flag.StringVar(&ssid, "ssid", "", "Defines the SSID of the Wifi (required)")
+	flag.StringVar(&password, "pass", "", "Defines the password of the Wifi (required)")
 	flag.Parse()
+
+	if ssid == "" || password == "" {
+		fmt.Println("Missing parameters")
+		flag.PrintDefaults()
+		return
+	}
 
 	qrcodeText := fmt.Sprintf("WIFI:T:WPA;S:%s;P:%s", ssid, password)
 
